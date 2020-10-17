2949dfea3a8b3398d2c767cc50e54a029143ac14
jwpark15 <jonnyp320@gmail.com>
2020-10-17 02:14:29 -0400

    added file for drawing on img

---
+++ draw_img.js
@@ -0,0 +1,3 @@
+import * as posenet from '@tensorflow-models/posenet';
+import Stats from 'stats.js'
+import {drawBoundingBox, drawKeypoints, drawSkeleton, toggleLoadingUI, tryResNetButtonName, tryResNetButtonText, updateTryResNetButtonDatGuiCss} from './utils';
