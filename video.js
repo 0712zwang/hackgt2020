bf54c550681ebd8cb673ce79afd6a2d51c1dfa4d
Zida Wang <0712zwang@gmail.com>
2020-10-17 19:13:14 -0400

    Update video.js

--- video.js
+++ video.js
@@ -43,7 +43,7 @@ async function setupVideo() {
 
   return new Promise(
 		(resolve) => {
-  		video.onloadedmetadata = () => {
+  		video.onloadeddata = () => {
     			resolve(video);
   		};
 		});
