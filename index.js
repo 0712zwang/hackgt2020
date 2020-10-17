1b8b9467132a9a91adec5887575a81730e73b8e2
Zida Wang <0712zwang@gmail.com>
2020-10-16 23:18:46 -0400

    trying stuff

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
-function poseEstimation(video) {
-	console.log("hahahhahaha")
+client_vid = null
+
+function updateFile() {
+	client_vid = document.getElementById('client_vid').value
+}
+
+function poseEstimate() {
+	var flipHorizontal = false;
+
+	var imageElement = document.getElementById('test_vid');
+
+	posenet.load().then(
+		function(net) {
+  			const pose = net.estimateSinglePose(
+  				imageElement, {
+    				flipHorizontal: true
+  				});
+  			return pose;
+		}).then(function(pose){
+  			console.log(pose);
+		}
+	)
 }
