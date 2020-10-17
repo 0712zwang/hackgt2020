0ccecd5c4479391a669a64f8537e9e2e32102a89
jwpark15 <jonnyp320@gmail.com>
2020-10-17 12:59:34 -0400

    test coordinates

--- draw_img.js
+++ draw_img.js
@@ -1,3 +1,22 @@
 import * as posenet from '@tensorflow-models/posenet';
 import Stats from 'stats.js'
 import {drawBoundingBox, drawKeypoints, drawSkeleton, toggleLoadingUI, tryResNetButtonName, tryResNetButtonText, updateTryResNetButtonDatGuiCss} from './utils';
+
+
+function drawOnImage() {
+	var flipHorizontal = false;
+
+	var imageElement = document.getElementById('test_image');
+
+	posenet.load().then(
+		function(net) {
+  			const pose = net.estimateSinglePose(
+  				imageElement, {
+    				flipHorizontal: true
+  				});
+  			return pose;
+		}).then(function(pose){
+  			console.log(pose);
+		}
+	)
+}
